cfebc3d637bb9948cb5dba4bfa02844e9c61ae8c
senthilkumar_td <senthil.kumar@photoninfotech.net>
2019-06-10 14:59:25 +0530

    platform nodemodues ios

--- deploy1.sh
+++ deploy1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/local/Cellar/node/12.1.0/bin/dus-deployer --config DeploymentConfig.json --platform ios --react16 true --updateGraphVersion 0.0.0.0 --outputPath output --prodUpdateGraph output/UpdatePatch.json --entry-file index.js
+/usr/local/Cellar/node/12.1.0/bin/dus-deployer --config DeploymentConfig.json --platform ios --react16 true --updateGraphVersion 0.0.0.0 --outputPath output --prodUpdateGraph output/UpdatePatch.json
 node deployUpdatePatch.js
 echo "Deployment done!!!"
--- ios/build/dusdemo.build/Debug-iphonesimulator/dusdemo.build/DerivedSources/dusdemo_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char dusdemoVersionString[];
- extern const double dusdemoVersionNumber;
-
- const unsigned char dusdemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:dusdemo  PROJECT:dusdemo-1" "\n";
- const double dusdemoVersionNumber __attribute__ ((used)) = (double)1.;
