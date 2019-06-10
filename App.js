16a4c2a15928e043cca3be11ec6832226a3f65ef
senthilkumar_td <senthil.kumar@photoninfotech.net>
2019-06-10 15:42:58 +0530

    new change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome</Text>
+        <Text>Welcome2</Text>
         <Text>Run sh deploy2.sh</Text>
         <Text>Relaunch your app once the update is deployed</Text>
         <Text>Your new changes should reflect now</Text>
